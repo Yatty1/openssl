f87436f68b6f1c52d79d6fa0e74b788b96201ef3
yatty <shin.ya.myad11@gmail.com>
2018-09-07 18:06:51 -0700

    removed extras

--- srcs/sha256_encrypt.c
+++ srcs/sha256_encrypt.c
@@ -12,7 +12,7 @@
 
 #include "ft_ssl.h"
 
-static const uint32_t	g_sk[64] = {
+static const uint32_t	g_k[64] = {
 	0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
 	0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
 	0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
@@ -84,7 +84,7 @@ t_sha256				*init_sha256(const char *str, int len)
 }
 
 /*
-**How to transform
+**	How to transform
 ** a message schedule of sixty four 32-bit words
 ** eight working variables of 32 bits each
 ** a hash value of eight 32 bits words
@@ -101,7 +101,7 @@ void					transform(t_sha256 *ob)
 	t = 0;
 	while (t < 64)
 	{
-		ob->t1 = ob->h[7] + ob->sigf[1](ob->h[4]) + ob->ch(ob->h) + g_sk[t] + ob->w[t];
+		ob->t1 = ob->h[7] + ob->sigf[1](ob->h[4]) + ob->ch(ob->h) + g_k[t] + ob->w[t];
 		ob->t2 = ob->sigf[0](ob->h[0]) + ob->ma(ob->h);
 		ob->h[7] = ob->h[6];
 		ob->h[6] = ob->h[5];
@@ -123,10 +123,6 @@ t_sha256				*transform_sha256(t_sha256 *ob)
 	offset = 0;
 	while (ob->chunk_n--)
 	{
-//		ft_memcpy(ob->e.c, ob->msg + offset, 64);
-//		t = -1;
-//		while (++t < 16)
-//			ob->w[t] = ob->e.m[t];
 		t = -1;
 		while (++t < 16)
 			ob->w[t] = ob->msg[offset + t * 4 + 0] << 24 | ob->msg[offset + t * 4 + 1] << 16
